d93cb8b593d8bc38b44295179c211861e08503fa
Kazuki Onodera <luvsic02@gmail.com>
2018-03-18 02:05:39 +0900

    update

--- py/run.py
+++ py/run.py
@@ -7,10 +7,16 @@ Created on Sat Mar 17 19:37:49 2018
 """
 
 import os
+from time import sleep
 import sys
 argv = sys.argv
 
 file = argv[1]
+if len(argv)>2:
+    minutes = 60 * int(argv[2])
+else:
+    minutes = 0
 
+sleep(minutes)
 os.system('nohup python -u {} > log.txt &'.format(file))
 
