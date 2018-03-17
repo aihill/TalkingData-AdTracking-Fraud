b46362941af79beba63e3c2b203183084840fbda
Kazuki Onodera <luvsic02@gmail.com>
2018-03-17 22:49:23 +0900

    update

--- py/101_click_time_diff.py
+++ py/101_click_time_diff.py
@@ -8,30 +8,19 @@ Created on Mon Mar 12 13:40:52 2018
 
 import numpy as np
 import pandas as pd
-from itertools import permutations
 from tqdm import tqdm
 import gc
 import utils
 
-col = list(permutations(['ip', 'app', 'device', 'os', 'channel']))
-col3 = list(permutations(['ip', 'app', 'device', 'os', 'channel'], 3))
-col2 = list(permutations(['ip', 'app', 'device', 'os', 'channel'], 2))
-col1 = list(permutations(['ip', 'app', 'device', 'os', 'channel'], 1))
-
-
-col += col3 + col2 + col1
-
-keys = list(col[0])+['click_time']
-
 
 # =============================================================================
-# for valid
+# for train
 # =============================================================================
-valid = utils.read_pickles('../data/valid').sort_values(utils.sort_keys) # be sure to sort by this keys
+df = utils.read_pickles('../data/train').sort_values(utils.sort_keys) # be sure to sort by this keys
 
 ip_bk = app_bk = device_bk = os_bk = channel_bk = click_time_bk = None
 li = []
-for ip, app, device, os, channel, click_time in tqdm(valid[keys].values, miniters=99999):
+for ip, app, device, os, channel, click_time in tqdm(df[utils.sort_keys].values, miniters=99999):
     
     if ip_bk is None:
         li.append(-1)
@@ -42,21 +31,21 @@ for ip, app, device, os, channel, click_time in tqdm(valid[keys].values, miniter
     
     ip_bk, app_bk, device_bk, os_bk, channel_bk, click_time_bk = ip, app, device, os, channel, click_time
 
-valid['same_ip-app-device-os-channel_diff'] = li
+df['same_ip-app-device-os-channel_diff'] = li
+df['hour'] = df.click_time.dt.hour + (df.click_time.dt.minute/60)
+df[['same_ip-app-device-os-channel_diff']].to_pickle('../data/101_train.p')
 
-valid[['same_ip-app-device-os-channel_diff']].to_pickle('../data/101_valid.p')
-
-del valid; gc.collect()
+del df; gc.collect()
 
 
 # =============================================================================
 # for test
 # =============================================================================
-test = utils.read_pickles('../data/test').sort_values(utils.sort_keys)
+df = utils.read_pickles('../data/test').sort_values(utils.sort_keys) # be sure to sort by this keys
 
 ip_bk = app_bk = device_bk = os_bk = channel_bk = click_time_bk = None
 li = []
-for ip, app, device, os, channel, click_time in tqdm(test[keys].values, miniters=99999):
+for ip, app, device, os, channel, click_time in tqdm(df[utils.sort_keys].values, miniters=99999):
     
     if ip_bk is None:
         li.append(-1)
@@ -67,9 +56,8 @@ for ip, app, device, os, channel, click_time in tqdm(test[keys].values, miniters
     
     ip_bk, app_bk, device_bk, os_bk, channel_bk, click_time_bk = ip, app, device, os, channel, click_time
 
-test['same_ip-app-device-os-channel_diff'] = li
+df['same_ip-app-device-os-channel_diff'] = li
+df['hour'] = df.click_time.dt.hour + (df.click_time.dt.minute/60)
+df[['same_ip-app-device-os-channel_diff']].to_pickle('../data/101_test.p')
 
-test[['same_ip-app-device-os-channel_diff']].to_pickle('../data/101_test.p')
-
-del test; gc.collect()
 
