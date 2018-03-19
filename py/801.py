bc282568a765f5e4d99f6ab689308089f61953c7
kazuki-onodera <kazuki.onodera@dena.com>
2018-03-19 15:39:59 +0900

    update

---
+++ py/801.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 19 15:21:44 2018
+
+@author: kazuki.onodera
+"""
+
+import numpy as np
+import pandas as pd
+import gc
+import utils
+utils.start(__file__)
+
+
+for keys in tqdm(utils.comb):
+    gc.collect()
+    keys_ = '-'.join(keys)
+    df = pd.merge(pd.read_pickle('../data/{}_count_old.p'.format(keys_)),
+                  pd.read_pickle('../data/{}_timestd_old.p'.format(keys_)),
+                  on=keys, how='outer')
+    df.to_pickle('../data/{}_feature.p'.format(keys_))
+    
