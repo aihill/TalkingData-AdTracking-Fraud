2e07cf1f559469f03e1076894f9c0e4c9374c88f
Kazuki Onodera <luvsic02@gmail.com>
2018-03-20 03:55:47 +0900

    update

--- py/801.py
+++ py/801.py
@@ -10,8 +10,9 @@ import numpy as np
 import pandas as pd
 from tqdm import tqdm
 import gc
+import xgboost as xgb
 import utils
-utils.start(__file__)
+#utils.start(__file__)
 
 # setting
 SEED = 48
@@ -24,28 +25,43 @@ np.random.seed(SEED)
 
 train = pd.concat([utils.read_pickles('../data/train'),
                    pd.read_pickle('../data/101_train.p'),
-                   pd.read_pickle('../data/102_train.p')], axis=1)#.sample(frac=0.4)
+                   pd.read_pickle('../data/102_train.p')]+[pd.read_pickle('../data/{}_train.p'.format('-'.join(keys))) for keys in utils.comb], 
+                  axis=1)#.sample(frac=0.4)
 
 gc.collect()
 
-for keys in tqdm(utils.comb):
-    gc.collect()
-    keys_ = '-'.join(keys)
-    train = pd.merge(train, pd.read_pickle('../data/{}_feature.p'.format(keys_)), 
-                     on=keys, how='left')
-
-train.drop(['click_time', 'attributed_time'], axis=1, inplace=True)
 
 y = train.is_attributed
-train.drop(['ip', 'app', 'device', 'os', 'channel', 'is_attributed'], 
+train.drop(['ip', 'app', 'device', 'os', 'channel', 'is_attributed', 'click_time', 'attributed_time'], 
            axis=1, inplace=True)
 train.fillna(-1, inplace=True)
 
-# =============================================================================
-# 
-# =============================================================================
+gc.collect()
 
 
+# =============================================================================
+# save
+# =============================================================================
+
+valid_seed = np.random.randint(99999)
+X_valid = train.sample(frac=0.05, random_state=valid_seed)
+y_valid = y.sample(frac=0.05, random_state=valid_seed)
+
+dval = xgb.DMatrix(X_valid, y_valid)
+dval.save_binary('../data/dval.mt')
+
+train.drop(X_valid.index, inplace=True)
+y = y.drop(X_valid.index)
+
+del dval, X_valid, y_valid; gc.collect()
+
+for i in range(10):
+    train_seed = np.random.randint(99999)
+    X_train = train.sample(frac=0.1, random_state=train_seed)
+    y_train = y.sample(frac=0.1, random_state=train_seed)
+    xgb.DMatrix(X_train, y_train).save_binary('../data/dtrain{}.mt'.format(i))
+    
+    gc.collect()
 
 
 
---
+++ py/901_cv_319-5.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 15 04:04:05 2018
+
+@author: Kazuki
+
+sudo sh -c "echo 1 > /proc/sys/vm/drop_caches"
+
+"""
+
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+import sys
+sys.path.append('/home/kazuki_onodera/Python')
+import xgbextension as ex
+import xgboost as xgb
+import gc
+import utils
+utils.start(__file__)
+
+seed = 71
+np.random.seed(seed)
+
+# =============================================================================
+# load train
+# =============================================================================
+
+train = pd.concat([utils.read_pickles('../data/train'),
+                   pd.read_pickle('../data/101_train.p'),
+                   pd.read_pickle('../data/102_train.p')]+[pd.read_pickle('../data/{}_train.p'.format('-'.join(keys))) for keys in utils.comb], 
+                  axis=1).sample(frac=0.4)
+
+gc.collect()
+
+
+y = train.is_attributed
+train.drop(['ip', 'app', 'device', 'os', 'channel', 'is_attributed', 'click_time', 'attributed_time'], 
+           axis=1, inplace=True)
+train.fillna(-1, inplace=True)
+
+gc.collect()
+
+print(train.columns.tolist())
+
+# =============================================================================
+# xgboost
+# =============================================================================
+
+
+param = {'colsample_bylebel': 0.8,
+         'subsample': 0.1,
+         'eta': 0.1,
+         'eval_metric': 'auc',
+         'max_depth': 4,
+         'objective': 'binary:logistic',
+         'silent': 1,
+         'tree_method': 'hist',
+         'nthread': 64,
+         'seed':71}
+
+
+gc.collect()
+
+
+#yhat, imp, ret = ex.stacking(train, y, param, 9999, nfold=5, esr=30)
+#
+#imp.to_csv('imp.csv', index=False)
+
+# =============================================================================
+# cv
+# =============================================================================
+
+dtrain = xgb.DMatrix(train, y)
+
+cv = xgb.cv(param, dtrain, 9999, 
+            nfold=5, early_stopping_rounds=50, verbose_eval=5)
+
+#==============================================================================
+utils.end(__file__)
+
