815c3c5473cbd9af57e65bcadc432613e36fd897
Kazuki Onodera <luvsic02@gmail.com>
2018-03-24 09:55:12 +0900

    update

--- py/902_predict_323-1.py
+++ py/902_predict_323-1.py
@@ -90,7 +90,7 @@ test = pd.concat([utils.read_pickles('../data/test_old'),
                    pd.read_pickle('../data/101_test_old.p'),
                    pd.read_pickle('../data/102_test_old.p')]+[pd.read_pickle('../data/{}_test.p'.format('-'.join(keys))) for keys in utils.comb], 
                   axis=1)
-test = test[~test.click_id.isnull()]
+test = test[~test.click_id.isnull()].reset_index(drop=True)
 test.drop_duplicates('click_id', keep='last', inplace=True) # last?
 
 print('test.shape should be 18790469:', test.shape)
