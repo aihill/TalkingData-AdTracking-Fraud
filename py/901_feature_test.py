4a3b3c869715c4881e9dd9e2e3b786c1bb751113
Kazuki Onodera <luvsic02@gmail.com>
2018-04-14 09:36:10 +0900

    update

--- py/901_feature_test.py
+++ py/901_feature_test.py
@@ -26,8 +26,8 @@ utils.start(__file__)
 SEED = 4308 # np.random.randint(9999) #int(sys.argv[1])
 NROUND = 300
 FRAC = 0.1
-SUBMIT_FILE_PATH = '../output/feature-test2.csv.gz'
-COMMENT = '101,102,103-1'
+SUBMIT_FILE_PATH = '../output/feature-test3.csv.gz'
+COMMENT = '003~5,101'
 EXE_SUBMIT = True
 
 
@@ -39,12 +39,12 @@ print('seed :', SEED)
 
 X = pd.concat([utils.read_pickles('../data/train').sample(frac=FRAC, random_state=SEED),
 #               utils.read_pickles('../data/002_train').sample(frac=FRAC, random_state=SEED),
-#               utils.read_pickles('../data/003_train').sample(frac=FRAC, random_state=SEED),
-#               utils.read_pickles('../data/004_train').sample(frac=FRAC, random_state=SEED),
-#               utils.read_pickles('../data/005_train').sample(frac=FRAC, random_state=SEED),
+               utils.read_pickles('../data/003_train').sample(frac=FRAC, random_state=SEED),
+               utils.read_pickles('../data/004_train').sample(frac=FRAC, random_state=SEED),
+               utils.read_pickles('../data/005_train').sample(frac=FRAC, random_state=SEED),
                utils.read_pickles('../data/101_train').sample(frac=FRAC, random_state=SEED),
-               utils.read_pickles('../data/102_train').sample(frac=FRAC, random_state=SEED),
-               utils.read_pickles('../data/103-1_train').sample(frac=FRAC, random_state=SEED),
+#               utils.read_pickles('../data/102_train').sample(frac=FRAC, random_state=SEED),
+#               utils.read_pickles('../data/103-1_train').sample(frac=FRAC, random_state=SEED),
 #               utils.read_pickles('../data/103-2_train').sample(frac=FRAC, random_state=SEED),
                ], axis=1)
 gc.collect()
