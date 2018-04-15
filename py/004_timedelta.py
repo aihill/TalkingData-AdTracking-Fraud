7c2273728b65c7df2821de52f8049a01a04f7510
Kazuki Onodera <luvsic02@gmail.com>
2018-04-15 12:52:40 +0900

    update

--- py/004_timedelta.py
+++ py/004_timedelta.py
@@ -19,7 +19,7 @@ from collections import defaultdict
 import utils
 utils.start(__file__)
 
-os.system('rm -rf ../data/003*')
+os.system('rm -rf ../data/004*')
 
 trte = pd.concat([utils.read_pickles('../data/train', ['ip', 'app', 'device', 'os', 'channel', 'click_time']),
                 utils.read_pickles('../data/test_old', ['ip', 'app', 'device', 'os', 'channel', 'click_time'])])
@@ -51,8 +51,8 @@ def multi(count_keys):
     
     result = pd.DataFrame(result, columns=['timedelta_'+count_keys_])
     
-    result.iloc[0:184903890].to_pickle('../data/003__{}_train.p'.format(count_keys_))
-    result.iloc[184903890:].to_pickle('../data/003__{}_test.p'.format(count_keys_))
+    result.iloc[0:184903890].to_pickle('../data/004__{}_train.p'.format(count_keys_))
+    result.iloc[184903890:].to_pickle('../data/004__{}_test.p'.format(count_keys_))
 
 
 pool = Pool(nthread)
@@ -66,14 +66,14 @@ del trte; gc.collect()
 # =============================================================================
 
 # train
-df = pd.concat([pd.read_pickle(f) for f in sorted(glob('../data/003__*_train.p'))], axis=1).reset_index(drop=True)
-utils.to_pickles(df, '../data/003_train', 10)
+df = pd.concat([pd.read_pickle(f) for f in sorted(glob('../data/004__*_train.p'))], axis=1).reset_index(drop=True)
+utils.to_pickles(df, '../data/004_train', 10)
 
 # test
-df = pd.concat([pd.read_pickle(f) for f in sorted(glob('../data/003__*_test.p'))], axis=1).reset_index(drop=True)
-utils.to_pickles(df, '../data/003_test', 10)
+df = pd.concat([pd.read_pickle(f) for f in sorted(glob('../data/004__*_test.p'))], axis=1).reset_index(drop=True)
+utils.to_pickles(df, '../data/004_test', 10)
 
-os.system('rm -rf ../data/003__*.p')
+os.system('rm -rf ../data/004__*.p')
 
 
 
