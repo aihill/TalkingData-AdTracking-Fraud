f4a7b9986a9e2f2a88bd333cf9c8093111436a59
Kazuki Onodera <luvsic02@gmail.com>
2018-04-27 20:15:48 +0900

    update

--- onodera/py/802_parameter_tuning.py
+++ onodera/py/802_parameter_tuning.py
@@ -33,13 +33,58 @@ system('rm SUCCESS_802')
 # =============================================================================
 # load
 # =============================================================================
+#dtrain = lgb.Dataset('../data/dtrain.mt')
+#gc.collect()
+#
+#
+#dvalid = lgb.Dataset('../data/dvalid.mt')
+#gc.collect()
 
-dtrain = lgb.Dataset('../data/dtrain.mt')
-gc.collect()
+# =============================================================================
+# load
+# =============================================================================
+categorical_feature = ['ip', 'app', 'device', 'os', 'channel', 'day', 'hour']
+
+print('concat train')
+load_files = sorted(glob('../data/*_train_sampling.p'))
+X = pd.concat([pd.read_pickle(f) for f in tqdm(load_files)], axis=1)
+print('X.isnull().sum().sum():', X.isnull().sum().sum())
+drop_feature = ['click_time', 'attributed_time']
+X.drop(drop_feature, axis=1, inplace=True)
+X.fillna(-1, inplace=True)
+
+print('train.shape:', X.shape )
 
 
-dvalid = lgb.Dataset('../data/dvalid.mt')
-gc.collect()
+y = X.is_attributed
+del X['is_attributed']; gc.collect()
+
+dtrain = lgb.Dataset(X, label=y, categorical_feature=categorical_feature)
+X_head = X.head()
+
+del X; gc.collect()
+
+
+
+
+
+print('concat valid')
+load_files = sorted(glob('../data/*_valid_sampling.p'))
+X = pd.concat([pd.read_pickle(f) for f in tqdm(load_files)], axis=1)
+print('X.isnull().sum().sum():', X.isnull().sum().sum())
+drop_feature = ['click_time', 'attributed_time']
+X.drop(drop_feature, axis=1, inplace=True)
+X.fillna(-1, inplace=True)
+
+print('valid.shape:', X.shape )
+
+
+y = X.is_attributed
+del X['is_attributed']; gc.collect()
+
+dvalid = lgb.Dataset(X[X_head.columns], label=y, categorical_feature=categorical_feature)
+
+del X; gc.collect()
 
 
 # =============================================================================
