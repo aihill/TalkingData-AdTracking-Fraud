9926f0ce14e2a3f9f088588f57eb2c9a9758d737
kazuki-onodera <kazuki.onodera@dena.com>
2018-05-10 14:29:21 +0900

    update

--- py/803_stepwise_510-1.py
+++ py/803_stepwise_510-1.py
@@ -72,7 +72,7 @@ utils.send_line('START {}'.format(__file__))
 # =============================================================================
 imp = pd.read_csv('imp_802_importance_510-1.py.csv').set_index('index')
 feature_all = imp[imp.weight!=0].index.tolist()
-feature_all = [c for c in feature_all if not (c.startswith('timedelta_') or c.startswith('timedelta2_'))]
+feature_all = [c for c in feature_all if not (c.startswith('timedelta_') or c.startswith('timedelta2_') or c.startwith('timeDD_'))]
 print(f'feature_all {feature_all}')
 
 X_train = pd.read_feather(f'../data/X_train_mini_s{TARGET_SEED}.f')[feature_all]
